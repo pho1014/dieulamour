871469c66aadc1004bbd98c5bcd2e8ff2004db37
pho1014 <phillip19902001@gmail.com>
2018-06-25 16:54:34 +1000

    stats

---
+++ kickstats/stats/KickStats/src/Fight.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fight {
+	private String mID;
+	private String mDate;
+	private String mFighter;
+	private String mOpponent;
+	public String mWeightClass;
+	private String mLocation;
+	private Double mDuration;
+	private List<Round> mRounds = new ArrayList<Round>();
+	
+	
+}
